f8cf25833bd89f621e58ab95f6e8dc2f5baeaef5
Sergey446128 <s.milyaev2018@yandex.ru>
2024-09-12 14:53:15 +0300

    Add files via upload

---
+++ module 3 5.py
@@ -0,0 +1,15 @@
+  # Рекурсивное умножение цифр:
+
+def get_multiplied_digits(number):
+    str_number = str(number)
+    if int(str_number[0]) != 0:
+     first = int(str_number[0])
+    else:
+        first = 1
+    if len(str_number) > 1:
+        return first * get_multiplied_digits(int(str_number[1:]))
+    else:
+        return first
+
+result = get_multiplied_digits(40203)
+print(result)
